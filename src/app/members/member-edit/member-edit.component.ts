79a5d7bb8f2966fdcc94ac88f83e7829d4145cf8
Hua Cai <huacai@10-200-199-59-eduroam.staff.utas.edu.au>
2019-10-08 09:52:59 +1100

    finish photo upload

--- src/app/members/member-edit/member-edit.component.ts
+++ src/app/members/member-edit/member-edit.component.ts
@@ -13,6 +13,7 @@ import {AuthService} from '../../_services/auth.service';
 })
 export class MemberEditComponent implements OnInit {
   user : User
+  photoUrl: string;
   @ViewChild('editForm',{static: true}) editForm : NgForm;
 
 
@@ -25,6 +26,11 @@ export class MemberEditComponent implements OnInit {
     this.route.data.subscribe(data=>{
       this.user= data['user'];
     })
+    this.authService.currentPhotoUrl.subscribe(
+      photoUrl=>{
+        this.photoUrl=photoUrl;
+      }
+    )
   }
 
   updateUser() {
