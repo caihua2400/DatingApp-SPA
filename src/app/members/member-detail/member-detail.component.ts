ad15af1a8b4387ff52560fb1b7673c02c4c998b4
Hua Cai <huacai@10-200-199-59-eduroam.staff.utas.edu.au>
2019-10-22 15:00:21 +1100

    messages complete

--- src/app/members/member-detail/member-detail.component.ts
+++ src/app/members/member-detail/member-detail.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {User} from '../../_models/user';
 import {UserService} from '../../_services/user.service';
 import {AlertifyService} from '../../_services/alertify.service';
 import {ActivatedRoute} from '@angular/router';
 import {NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions} from 'ngx-gallery';
+import {TabsetComponent} from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-member-detail',
@@ -14,12 +15,18 @@ export class MemberDetailComponent implements OnInit {
   galleryOptions: NgxGalleryOptions[];
   galleryImages: NgxGalleryImage[];
   user:User
+  @ViewChild('memberTabs',{static : true}) memberTabs : TabsetComponent
 
   constructor(private userService : UserService, private alertify: AlertifyService, private route : ActivatedRoute) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.user = data['user'];
+    });
+
+    this.route.queryParams.subscribe(params =>{
+       const selectedTab=params['tab'];
+       this.memberTabs.tabs[selectedTab > 0 ? selectedTab : 0].active =true;
     });
 
     this.galleryOptions = [
@@ -49,5 +56,9 @@ export class MemberDetailComponent implements OnInit {
       });
     }
     return imageUrls;
+  }
+
+  selectTab(tabId : number){
+     this.memberTabs.tabs[tabId].active = true;
   }
 }
--- src/app/members/member-messages/member-messages.component.ts
+++ src/app/members/member-messages/member-messages.component.ts
@@ -3,6 +3,7 @@ import {Message} from '../../_models/message';
 import {UserService} from '../../_services/user.service';
 import {AuthService} from '../../_services/auth.service';
 import {AlertifyService} from '../../_services/alertify.service';
+import {tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-member-messages',
@@ -13,6 +14,7 @@ export class MemberMessagesComponent implements OnInit {
 
   @Input() recipientId : number;
   messages : Message[];
+  newMessage : any ={};
 
   constructor(private userService : UserService, private authService : AuthService, private alertifyService : AlertifyService) {
 
@@ -23,12 +25,35 @@ export class MemberMessagesComponent implements OnInit {
   }
 
   loadMessages(){
+    const currentId= +this.authService.decodedToken.nameid;
     this.userService.getMessageThread(this.authService.decodedToken.nameid,this.recipientId)
+      .pipe(
+          tap(
+           messages=>{
+                for(let i =0 ; i<messages.length; i++){
+                    if(messages[i].isRead === false && messages[i].recipientId === currentId){
+                         this.userService.markAsRead(currentId, messages[i].id);
+                    }
+                }
+           }
+          )
+      )
       .subscribe(messages =>{
         this.messages=messages;
       }, error => {
         this.alertifyService.error(error);
       });
+  }
+
+  sendMessage(){
+    this.newMessage.recipientId= this.recipientId;
+    this.userService.sendMessage(this.authService.decodedToken.nameid,this.newMessage).subscribe((message : Message) =>{
+
+         this.messages.unshift(message);
+         this.newMessage.content='';
+    }, error => {
+      this.alertifyService.error(error);
+    });
   }
 
 }
